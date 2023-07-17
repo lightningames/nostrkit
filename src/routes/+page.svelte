58c035c3465ca9865589c0e805c5b07783e01f06
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-07-17 11:46:44 -0700

    rebase

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "@picocss/pico";
+</script>
+
+<main class="container">
+  <header>
+    <nav>
+      <ul>
+        <li><strong>Brand</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </nav>
+  </header>
+  <hr />
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</main>
